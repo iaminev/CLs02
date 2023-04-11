9cceec070144994b52b8b1b336417b6c42882574
Ilshat Aminev <i.aminev@gmail.com>
2023-04-11 20:01:04 +0300

    Tasks 13 and 15

---
+++ Task13/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+int Get3dDigitAsNumber(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    int result = number % 10;
+    return result;
+}
+
+char Get3dDigitAsChar(int number)
+{
+    string NumberAsAtring = Convert.ToString(number);
+    return NumberAsAtring[2];
+}
+
+System.Console.Write("Enter an integer: ");
+int getNumber = Convert.ToInt32(Console.ReadLine()!);
+if (getNumber >= 100)
+{
+    int thirdDigitAsNumber = Get3dDigitAsNumber(getNumber);
+    System.Console.WriteLine($"The third digit of {getNumber} is {thirdDigitAsNumber} - found as number");
+    char thirdDigitAsChar = Get3dDigitAsChar(getNumber);
+    System.Console.WriteLine($"The third digit of {getNumber} is {thirdDigitAsChar} - found as char");
+}
+else
+{
+    System.Console.WriteLine($"There is no 3rd digit in {getNumber}");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+bool ItIsHoliday(int number)
+{
+    bool result = false;
+    if (number >= 1 && number <= 5)
+    {
+        result = false;
+    }
+    else
+    {
+        result = true;
+    }
+    return result;
+}
+
+System.Console.Write("Enter number of the day of the week: ");
+int getNumber = Convert.ToInt32(Console.ReadLine()!);
+if ((getNumber >= 1 && getNumber <= 7))
+{
+    bool itIsHoliday = ItIsHoliday(getNumber);
+    System.Console.WriteLine($"The day of the week number {getNumber} is holiday: {itIsHoliday}");
+}
+else
+{
+    System.Console.WriteLine($"No such day pf the week!");
+}
